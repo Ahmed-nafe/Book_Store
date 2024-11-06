28b67b608f28439a682450e14d8086d0be90855b
Ahmed Nafe <abonafe27@gmail.com>
2024-11-06 16:41:00 +0200

    Create Newest books method

--- lib/feature/presetation/screens/home_screen/manger/newestbooks/newest_books_cubit.dart
+++ lib/feature/presetation/screens/home_screen/manger/newestbooks/newest_books_cubit.dart
@@ -1,16 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:book/feature/presetation/screens/home_screen/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
-
 import '../../data/model/BookModel.dart';
-
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
   NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
   HomeRepo homeRepo;
 
-  Future<void> fetchFeaturedBooks() async {
+  Future<void> fetchNewestBooks() async {
     emit(NewestBooksLoading());
     var result = await homeRepo.fetchFeaturedBooks();
     result.fold((failure) => {emit(NewestBooksError(failure.errorMessage))},
